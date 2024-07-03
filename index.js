ab128ec1666729b470b1be19cb9dfcae68d619cc
Navneet <navneet11vinod11@gmail.com>
2024-07-03 19:45:14 +0530

    port for glitch

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ app.get('/weather/:latlon', async (request, response) => {
 
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
